1d29bfaefc718a86e5d377c9dc9a59317ed62038
Shazreen Seatter <71439994+ShazSeatter@users.noreply.github.com>
2025-12-12 16:54:07 +0000

    more crud operations.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,8 +6,20 @@ const prisma = new PrismaClient();
 const seed = async () => {
    await prisma.user.createMany({
     data: [
-        {name: 'Carol', email: 'carol@example.com', role: 'OWNER'},
-        {name: 'Karen', email: 'karen@example.com', role: "VIEWER"},
+        {name: 'Jacob', email: 'jacob@example.com', role: 'VIEWER', age: 23},
+        {name: 'Edward', email: 'edward@example.com', role: "EDITOR", age: 30},
+        {name: 'Carol', email: 'Carol@example.com', role: "ADMIN", age: 17},
+        {name: 'Karen', email: 'Karen@example.com', role: "ADMIN", age: 35},
+        {name: 'Susan', email: 'Susan@example.com', role: "ADMIN", age: 20},
+        {name: 'Jane', email: 'Jane@example.com', role: "VIEWER", age: 30},
+        {name: 'John', email: 'John@example.com', role: "OWNER", age: 25},
+        {name: 'Jack', email: 'Jack@example.com', role: "OWNER", age: 35},
+        {name: 'Shaz', email: 'Shaz@example.com', role: "EDITOR", age: 29},
+        {name: 'James', email: 'James@example.com', role: "ADMIN", age: 30},
+        {name: 'Paul', email: 'Paul@example.com', role: 'VIEWER', age: 34},
+        {name: 'Jill', email: 'Jill@example.com', role: "OWNER", age: 40},
+        {name: 'Jared', email: 'Jared@example.com', role: "EDITOR", age: 45},
+        {name: 'Lisa', email: 'Lisa@example.com', role: "VIEWER", age: 50},
     ]
    })
 }
--- server.ts
+++ server.ts
@@ -22,6 +22,130 @@ const init = async () => {
     //     }
     // });
 
+    // Slightly more complex query
+    //     server.route({
+    //     method: 'GET',
+    //     path: '/users',
+    //     handler: async (_request, h) => { 
+    //         const users = await prisma.user.findMany({
+    //         where: { role: "VIEWER", age: {
+    //             lte: 30
+    //         }}
+    //     });
+
+    //     return users;
+    //     }
+    // });
+
+    // OR test
+    // server.route({
+    //     method: 'GET',
+    //     path: '/users',
+    //     handler: async (_request, h) => { 
+    //         const users = await prisma.user.findMany({
+    //         where: { 
+    //             OR: [
+    //                 {
+    //                     role: 'EDITOR'
+    //                 }, {
+    //                     age: { lte: 30}
+    //                 }
+    //             ]
+    //         }
+    //     });
+
+    //     return users;
+    //     }
+    // });
+
+    // Negation
+    //     server.route({
+    //     method: 'GET',
+    //     path: '/users',
+    //     handler: async (_request, h) => { 
+    //         const users = await prisma.user.findMany({
+    //         where: { 
+    //            role: { not: 'ADMIN'}
+    //         }
+    //     });
+
+    //     return users;
+    //     }
+    // });
+
+    // Query where fields match certain values
+        server.route({
+        method: 'GET',
+        path: '/users',
+        handler: async (_request, h) => { 
+            const users = await prisma.user.findMany({
+            where: { 
+               role: { in: ["ADMIN", 'EDITOR', "OWNER"] },
+               age: { in: [20, 25, 30]}
+            }
+        });
+
+        return users;
+        }
+    });
+
+    // PUT - UPDATE 
+    // can only detect users by their unique fields
+        server.route({
+        method: 'PUT',
+        path: '/update-user',
+        handler: async (_request, h) => { 
+            const updatedUsers = await prisma.user.update({
+                where: {
+                    id: 19
+                },
+                data: {
+                    age: 35, 
+                    role: "ADMIN"
+                }
+        });
+
+        return updatedUsers;
+        }
+    });
+
+    
+    // DEL - DELETE 
+    // can only detect users by their unique fields
+    // Realworld - delete based on expiry dates, 
+    // delete Many - pass in a condition 
+        server.route({
+        method: 'DELETE',
+        path: '/delete-user',
+        handler: async (_request, h) => { 
+            const deletedUser = await prisma.user.delete({
+                where: {
+                    id: 19
+                },
+        });
+
+        return deletedUser;
+        }
+    });
+
+    // doesn't have to be unique field
+    // returns a count 
+        server.route({
+        method: 'DELETE',
+        path: '/delete-users',
+        handler: async (_request, h) => { 
+            const deletedUser = await prisma.user.deleteMany({
+                where: {
+                    age: {gt: 40}
+                },
+        });
+
+        return deletedUser;
+        }
+    });
+
+
+
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
